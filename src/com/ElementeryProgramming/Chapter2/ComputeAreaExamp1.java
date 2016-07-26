a46d4597b110191b4802628e2c99482fd55a6cef
Jagannath <jagannatham66@gmail.com>
2016-07-26 17:07:55 +0530

    Java and Selenium intial project

---
+++ src/com/ElementeryProgramming/Chapter2/ComputeAreaExamp1.java
@@ -0,0 +1,19 @@
+package com.ElementeryProgramming.Chapter2;
+
+public class ComputeAreaExamp1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Step 1: Read in radius
+		// Step 2: Compute area
+		// Step 3: Display the area
+
+		double radius=11;
+		double area;
+		//The program declares radius and area as variables
+		//The reserved word double indicates that radius and area are floating-point values stored in the computer
+		area=3.14*radius*radius;
+		System.out.println("The area of the Circle is: " +area);
+	}
+
+}
---
+++ src/com/ElementeryProgramming/Chapter2/ComputeAreaWithConsoleInput.java
@@ -0,0 +1,10 @@
+package com.ElementeryProgramming.Chapter2;
+
+public class ComputeAreaWithConsoleInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
