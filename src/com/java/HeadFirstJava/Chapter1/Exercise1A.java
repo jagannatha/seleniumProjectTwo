b3bcb493f2d5a8c9b685a5bab2c752f7212be410
Jagannath <jagannatham66@gmail.com>
2016-07-28 17:20:33 +0530

    java exersise

--- src/com/java/HeadFirstJava/Chapter1/Exercise1A.java
+++ src/com/java/HeadFirstJava/Chapter1/Exercise1A.java
@@ -1,16 +1,16 @@
 package com.java.HeadFirstJava.Chapter1;
 
 public class Exercise1A {
-public static void main(String[] args) {
-	  int x = 1;
-      while (x < 10) {
-    	  x=x+1;
-          if(x>3) {
-              System.out.println("big x");
-		}
-		 
-	}
-}
+	public static void main(String[] args) {
+		int x = 1;
+		while (x < 10) {
+			x=x+1;
+			if(x>3) {
+				System.out.println("big x");
+			}
 
- 
+		}
+	}
+
+
 }
--- src/com/java/HeadFirstJava/Chapter1/testmatch.java
+++ src/com/java/HeadFirstJava/Chapter1/testmatch.java
@@ -1,21 +1,22 @@
 package com.java.HeadFirstJava.Chapter1;
 
 public class testmatch {
-public static void main(String[] args) {
-	
-	int x=0;
-	int y=0;
-	while(x<5){
-		//y=y+x; //match with output 00 11 23 36 410
-		//y=x-y;  //00 11 21 32 42 
-		y=y+2;
-		if(y>4){
-			y=y-1;
+	public static void main(String[] args) {
+
+		int x=0;
+		int y=0;
+		while(x<5){
+			//	Option1	y=y+x; //match with output 00 11 23 36 410
+			//	Option 2	y=x-y;  //00 11 21 32 42 
+			//	option 3	y=y+2;//02 14 25 36 47
+
+			//		if(y>4){
+			//			y=y-1;
+			//		}
+
+			System.out.println( x+"" +y+ "" );
+			x=x+1;
 		}
-				
-		System.out.println( x+"" +y+ "" );
-		x=x+1;
 	}
-}
-	
+
 }
